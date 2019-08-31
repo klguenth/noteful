bbfb21b57eec3e7b78a660a1e60647b4c467d239
klguenth <klguenth@gmail.com>
2019-08-31 18:05:56 -0500

    created Header, Main, and Sidebar. Added BrowserRouter to index.js

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <h1>Noteful</h1>
+    )
+}
