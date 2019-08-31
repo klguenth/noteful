76c220a4084456089562b1ae17a608a46b594118
klguenth <klguenth@gmail.com>
2019-08-31 15:56:58 -0500

    added initial code to App and created dummy store file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Sidebar>
+          <Route path='/' component={MainSidebar} />
+          <Route path='/foo' component={FooSidebar} />
+        </Sidebar>
+        <Main>
+          <Route path='/' component={MainMain} />
+          <Route path='/foo' component={FooMain} />
+        </Main>
+      </div>
+    );
+  }
 }
-
-export default App;
