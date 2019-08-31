f448811a55ab9f30c27268a93b1536c5ca4cbeb3
klguenth <klguenth@gmail.com>
2019-08-31 18:12:45 -0500

    updated App.js and created Note.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Header from './Header';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header>
+          <Route path='/' component={Header} />
+        </Header>
         <Sidebar>
           <Route path='/' component={MainSidebar} />
           <Route path='/foo' component={FooSidebar} />
         </Sidebar>
         <Main>
-          <Route path='/' component={MainMain} />
+          <Route path='/' component={FooFoo} />
           <Route path='/foo' component={FooMain} />
         </Main>
       </div>
