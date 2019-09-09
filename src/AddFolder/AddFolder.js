1c0113452c87585a2c2cdf40a049acd5935e3e11
klguenth <klguenth@gmail.com>
2019-09-09 02:48:42 -0500

    created AddFolder and AddNote

---
+++ src/AddFolder/AddFolder.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default class AddFolder extends React.Component {
+    
+    static defaultProps ={
+        addFolder: () => {},
+      }
+      static contextType = ApiContext;
+    
+      handleAddFolder = e => {
+        e.preventDefault()
+        const folderId = this.props.id
+    
+    fetch(`${config.API_ENDPOINT}/folders/${folderId}`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+      })
+        .then(res => {
+          if (!res.ok)
+            return res.json().then(e => Promise.reject(e))
+          return res.json()
+        })
+        .then(() => {
+          this.context.addFolder(folderId)
+          // allow parent to perform extra behaviour
+          this.props.addFolder(folderId)
+        })
+        .catch(error => {
+          console.error({ error })
+        });
+
+    render() {
+        return (
+            <form className="createFolder">
+                <h2>Add Folder</h2>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" className="name" name="name" id="name" />
+                </div>
+                <div className="createFolderButtons">
+                    <button type="reset" className="buttonReset">
+                        Cancel
+                    </button>
+                    <button type="submit" className="buttonSubmit">
+                        Save
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}}
---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export default class AddNote extends React.Component {
+        
+    static defaultProps ={
+        addNote: () => {},
+      }
+      static contextType = ApiContext;
+    
+      handleAddNote = e => {
+        e.preventDefault()
+        const noteId = this.props.id
+    
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+      })
+        .then(res => {
+          if (!res.ok)
+            return res.json().then(e => Promise.reject(e))
+          return res.json()
+        })
+        .then(() => {
+          this.context.addNote(noteId)
+          // allow parent to perform extra behaviour
+          this.props.addNote(noteId)
+        })
+        .catch(error => {
+          console.error({ error })
+        });
+
+    validateName() {
+        const name = "this.state.name.value.trim()"
+        if (name.length === 0) {
+            return 'Name is required';
+        }
+    }
+    
+    render() {
+        return (
+            <form className="createNote">
+                <h2>Add Note</h2>
+                <div className="form-group">
+                    <div className="noteHint">* required</div>
+                    <label htmlFor="name">Name*</label>
+                    <input type="text" className="noteName" name="name" id="noteName" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="content">Content</label>
+                    <input type="text" className="noteContent" name="content" id="noteContent" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="folderSelect">Folder Select</label>
+                </div>
+
+                <div className="note-button">
+                    <button type="reset" className="noteButtonReset">
+                        Cancel
+                    </button>
+                    <button type="submit" className="noteButtonSubmit">
+                        Save
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}}
