62a05f02a0c9f832ee5bc94f0efd8da7c2fc2b03
klguenth <klguenth@gmail.com>
2019-09-10 06:57:55 -0500

    added back button functionality

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ApiContext from '../ApiContext';
 import config from '../config';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import CircleButton from '../CircleButton/CircleButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { findNote, findFolder } from '../notes-helpers';
 
 export default class AddFolder extends React.Component {
     
@@ -39,7 +42,12 @@ export default class AddFolder extends React.Component {
       }
 
     render() {
+      const { notes, folders, } = this.context
+      const { noteId } = this.props.match.params
+      const note = findNote(notes, noteId) || {}
+      const folder = findFolder(folders, note.folderId)
         return (
+          <div>
             <form className="createFolder">
                 <h2>Add Folder</h2>
                 <div className="form-group">
@@ -47,14 +55,29 @@ export default class AddFolder extends React.Component {
                     <input type="text" className="name" name="name" id="name" />
                 </div>
                 <div className="createFolderButtons">
-                    <button type="reset" className="buttonReset">
-                        Cancel
-                    </button>
                     <button type="submit" className="buttonSubmit">
                         Save
                     </button>
                 </div>
             </form>
+            <div className='NotePageNav'>
+            <CircleButton
+              tag='button'
+              role='link'
+              onClick={() => this.props.history.goBack()}
+              className='NotePageNav__back-button'
+            >
+              <FontAwesomeIcon icon='chevron-left' />
+              <br />
+              Back
+            </CircleButton>
+            {folder && (
+              <h3 className='NotePageNav__folder-name'>
+                {folder.name}
+              </h3>
+            )}
+          </div>
+        </div>
         );
     }
 }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ApiContext from '../ApiContext';
 import config from '../config';
+import CircleButton from '../CircleButton/CircleButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { findNote, findFolder } from '../notes-helpers';
 
 export default class AddNote extends React.Component {
         
@@ -46,31 +49,51 @@ export default class AddNote extends React.Component {
     }
     
     render() {
+        const { notes, folders, } = this.context
+        const { noteId } = this.props.match.params
+        const note = findNote(notes, noteId) || {}
+        const folder = findFolder(folders, note.folderId)
         return (
-            <form className="createNote">
-                <h2>Add Note</h2>
-                <div className="form-group">
-                    <div className="noteHint">* required</div>
-                    <label htmlFor="name">Name*</label>
-                    <input type="text" className="noteName" name="name" id="noteName" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="content">Content</label>
-                    <input type="text" className="noteContent" name="content" id="noteContent" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="folderSelect">Folder Select</label>
-                </div>
+            <div>
+                <form className="createNote">
+                    <h2>Add Note</h2>
+                    <div className="form-group">
+                        <div className="noteHint">* required</div>
+                        <label htmlFor="name">Name*</label>
+                        <input type="text" className="noteName" name="name" id="noteName" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="content">Content</label>
+                        <input type="text" className="noteContent" name="content" id="noteContent" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="folderSelect">Folder Select</label>
+                    </div>
 
-                <div className="note-button">
-                    <button type="reset" className="noteButtonReset">
-                        Cancel
-                    </button>
-                    <button type="submit" className="noteButtonSubmit">
-                        Save
-                    </button>
+                    <div className="note-button">
+                        <button type="submit" className="noteButtonSubmit">
+                            Save
+                        </button>
+                    </div>
+                </form>
+                <div className='NotePageNav'>
+                <CircleButton
+                    tag='button'
+                    role='link'
+                    onClick={() => this.props.history.goBack()}
+                    className='NotePageNav__back-button'
+                >
+                    <FontAwesomeIcon icon='chevron-left' />
+                    <br />
+                    Back
+                </CircleButton>
+                {folder && (
+                    <h3 className='NotePageNav__folder-name'>
+                    {folder.name}
+                    </h3>
+                )}
                 </div>
-            </form>
+            </div>
         )
     }
 }
