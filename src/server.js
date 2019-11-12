0b3569dbf212c356193da1d9ba3acb3223306cd7
klguenth <klguenth@gmail.com>
2019-11-12 03:15:23 -0600

    fixed knex instance issue in folders-router

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const foldersRouter = require('./folders-router')
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan')
+const knex = require('knex')
 const { PORT, DB_URL } = require('./config')
 console.log('Port equals', PORT)
 console.log(process.env.API_TOKEN)
@@ -10,6 +11,12 @@ const app = express();
 app.use(morgan('dev'));
 app.use('/api', foldersRouter);
 
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+})
+
+app.set('db', db)
 
 app.get('/', (req, res) => {
     res.send('Noteful Express');
