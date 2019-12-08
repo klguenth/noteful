492f4c4915836884b5ce82b37328d09a14e98453
klguenth <klguenth@gmail.com>
2019-12-08 12:37:04 -0600

    udpated code

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     API_ENDPOINT: 'http://localhost:8000/api',
     PORT: 8000,
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
-    TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
