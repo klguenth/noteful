fa052949961dc73e55a026a77e54f30e3737475e
klguenth <klguenth@gmail.com>
2019-12-08 09:55:33 -0600

    updated dependencies and ensured working tests

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     API_ENDPOINT: 'http://localhost:8000/api',
     PORT: 8000,
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
