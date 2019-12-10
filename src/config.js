dc0cf848801d8e1acf3466fca9c610d4844b5f00
klguenth <klguenth@gmail.com>
2019-12-09 20:16:50 -0600

    final commit pre-deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     API_ENDPOINT: 'http://localhost:8000/api',
     PORT: 8000,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://kwxidghyienoxz:15ae8e1a9219bd33f53cd882b68d85d0bb59939b4e7c385616cd14a84ef903c4@ec2-107-20-234-175.compute-1.amazonaws.com:5432/d9htmus4sratue',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
